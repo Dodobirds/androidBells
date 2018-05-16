69072335ef41e57091bbf67b5b0b543159d187ad
Dodobirds <01010011c@gmail.com>
2018-05-16 19:54:57 -0400

    endscreen button

--- app/src/main/java/dodo/androidbells/EndScreenActivity.java
+++ app/src/main/java/dodo/androidbells/EndScreenActivity.java
@@ -1,13 +1,31 @@
 package dodo.androidbells;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class EndScreenActivity extends AppCompatActivity {
+public class EndScreenActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_screen);
+
+        button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view)  {
+        Intent intent = new Intent(this, GameActivity.class);
+        switch(view.getId())    {
+            case R.id.button2:
+                startActivity(intent);
+        }
     }
 }
